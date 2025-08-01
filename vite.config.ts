e36a7f783d3130f8e24398aee7a9933d49ab67c7
rockerBOO <rockerboo@gmail.com>
2025-07-31 21:22:08 -0400

    Add biome/stylelint/checker plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { fileURLToPath } from "node:url";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import biome from "vite-plugin-biome";
+import checker from "vite-plugin-checker";
+import stylelint from "vite-plugin-stylelint";
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), stylelint(), biome(), checker({ typescript: true })],
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
 		},
-	},
-	server: {
-		allowedHosts: ["hostmachine"],
 	},
 });
